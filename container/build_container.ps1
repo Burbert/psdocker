9683548843d4f8ed7345b89dc8f01f2a2939a104
Christoph Burmeister <cburmeister@visorian.com>
2020-11-20 00:03:38 +0100

    refactor build script #build

--- container/build_container.ps1
+++ container/build_container.ps1
@@ -1,29 +1,29 @@
+# login to azure using azure cli
 az login --service-principal --username $env:AZ_CLIENT_ID --password "$($env:AZ_CLIENT_SECRET)" --tenant $env:AZURE_TENANT_ID
 az account set --subscription $env:AZURE_SUBSCRIPTION_ID
 az acr login --name $env:AZURE_ACR_URL
 
-# Set-Location -Path "$($env:GITHUB_WORKSPACE)/container/module"
-
-# docker build -t psmodule:latest .
-# docker tag psmodule:latest "$($env:AZURE_ACR_URL)/psmodule/psmodule:latest"
-# docker push "$($env:AZURE_ACR_URL)/psmodule/psmodule:latest"
-
-
-
-
+# iterate through all folders in /container to identify if Dockerfile is present
 foreach ($folder in (Get-ChildItem -Path "$($env:GITHUB_WORKSPACE)/container" -Directory)){
     $folder_content = Get-ChildItem -Path $folder.FullName
     if('Dockerfile' -in $($folder_content.name)){
         Write-Output "processing folder: `'$($folder.FullName)`'"
-        $version = Get-Content -Path "$($folder.FullName)\version.json" | ConvertFrom-Json | Select-Object -ExpandProperty version
-        # Write-Output "version: `'$($folder.name)`'"
+        try{
+            $version = Get-Content -Path "$($folder.FullName)\version.json" -ErrorAction Stop | ConvertFrom-Json | Select-Object -ExpandProperty version
+        } catch {
+            Write-Warning "Version file not found - skipping folder: `'$($folder.FullName)`'"
+            continue
+        }
 
+        # set location folder
         Set-Location -Path $folder.FullName
 
+        # build container with version as tag
         docker build -t "$($folder.name):$($version)" .
         docker tag "$($folder.name):$($version)" "$($env:AZURE_ACR_URL)/$($folder.name)/$($folder.name):$($version)"
         docker push "$($env:AZURE_ACR_URL)/$($folder.name)/$($folder.name):$($version)"
         
+        # build container with lastest as tag
         docker build -t "$($folder.name):latest" .
         docker tag "$($folder.name):latest" "$($env:AZURE_ACR_URL)/$($folder.name)/$($folder.name):latest"
         docker push "$($env:AZURE_ACR_URL)/$($folder.name)/$($folder.name):latest"
