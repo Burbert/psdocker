d61a4a63c1885e26501cdaeffe726f0f544c9573
Christoph Burmeister <cburmeister@visorian.com>
2020-11-20 14:34:26 +0100

    changed microservice run script // #build

--- container/microservice/web.ps1
+++ container/microservice/web.ps1
@@ -3,26 +3,18 @@ Start-PodeServer {
     Add-PodeEndpoint -Address * -Port 80 -Protocol Http
 
     Add-PodeRoute -Method Get -Path '/health' -ScriptBlock {             
-        # Write-PodeTextResponse -Value 'Hello from PowerShell in Docker'
-
         Write-PodeJsonResponse -Value @{ 'response' = 'everything looks fine' } -StatusCode 200
     }
 
     Add-PodeRoute -Method Get -Path '/ping' -ScriptBlock {             
-        # Write-PodeTextResponse -Value 'Hello from PowerShell in Docker'
-
         Write-PodeJsonResponse -Value @{ 'response' = 'pong' } -StatusCode 200
     }
 
     Add-PodeRoute -Method Get -Path '/hello' -ScriptBlock {             
-        # Write-PodeTextResponse -Value 'Hello from PowerShell in Docker'
-
         Write-PodeJsonResponse -Value @{ 'response' = 'hey ho!' } -StatusCode 200
     }
 
     Add-PodeRoute -Method Get -Path '/error' -ScriptBlock {             
-        # Write-PodeTextResponse -Value 'Hello from PowerShell in Docker'
-
         Write-PodeJsonResponse -Value @{ 'response' = 'Error - everything is broken!' } -StatusCode 400
     }
 }
